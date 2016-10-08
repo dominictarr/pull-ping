9f9db69e19f919e6a7e37f97a5425bfdffbb961f
Dominic Tarr <dominic.tarr@gmail.com>
2016-10-08 22:42:11 +1300

    statistics@3 is now a reduce function

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var Pushable = require('pull-pushable')
-var Stats = require('statistics')
-var pull = require('pull-stream')
+var stats = require('statistics')
+var Drain = require('pull-stream/sinks/drain')
 
 module.exports = function (opts) {
   var timeout = opts && opts.timeout || 5*60*1000 //default: 5 minutes
   var serve = false, timer
   var source = Pushable()
-  var rtt = Stats(), skew = Stats()
+  var rtt = null, skew = null
 
   function ping () {
     //serve the ping pong, opponent
@@ -19,15 +19,16 @@ module.exports = function (opts) {
   //we send the first ping
   if(opts && opts.serve) ping()
 
-  return {
+  var self
+  return self = {
     source: source,
-    sink: pull.drain(function (remote_ts) {
+    sink: Drain(function (remote_ts) {
       if(serve) {
         var ts2 = Date.now()
-        rtt.value(ts2 - ts)
+        self.rtt = stats(self.rtt, ts2 - ts)
         //if their time is behind half a round trip behing ts2
         //consider that to be negative skew.
-        skew.value(remote_ts - ((ts2 + ts)/2))
+        self.skew = stats(self.skew, remote_ts - ((ts2 + ts)/2))
         serve = false
       }
       else {
